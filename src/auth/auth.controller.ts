680294f88362fe275bec9e70af6960f0077c6679
Nui-backend-intern <81944147+woraphatman@users.noreply.github.com>
2022-05-30 17:45:44 +0700

    try

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Post, Request,Get, UseGuards } from '@nestjs/common';
+import { Controller, Post,Get, UseGuards } from '@nestjs/common';
 import { timingSafeEqual } from 'crypto';
+import { Userdto } from 'src/user/dto/user.dto';
+import { User } from 'src/user/user.decorator';
+
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './jwt/jwt-auth.guard';
 import { LocalAuthGuard } from './local/local.auth.guards';
@@ -9,14 +12,14 @@ export class AuthController {
     constructor(private authService: AuthService){}
     @UseGuards(LocalAuthGuard)
     @Post ('login')
-    async login(@Request()  req:any): Promise<any>{
-            return this.authService.login(req.user)
+    async login(@User() user: Userdto){
+            return this.authService.login(user)
         }
    
 
 @UseGuards(JwtAuthGuard)
 @Get('profile')
-getProfile(@Request() req:any): Promise<any>{
-  return req.user;
+getProfile(@User() user: Userdto){
+  return user ;
 }
 }
