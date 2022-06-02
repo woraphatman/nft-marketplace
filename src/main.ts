2bec2747b70f611f8e42d59601480e29f51d943c
Nui-backend-intern <81944147+woraphatman@users.noreply.github.com>
2022-06-02 11:51:58 +0700

    3

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { MovieModule } from './movie/movie.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use()
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 3000);
